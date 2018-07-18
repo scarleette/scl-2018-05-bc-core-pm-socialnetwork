1adb000377d626bd5ae606dcc9b750777f40f097
Scarlett <scar.antoine@gmail.com>
2018-07-18 18:35:05 -0400

    Se crea funcion para obtener usuario logueado de database y mostraro en perfil

--- src/pages/perfil/app.js
+++ src/pages/perfil/app.js
@@ -1,11 +1,35 @@
-function perfil() {
+//Funcionalidad Menú
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+function perfil(user) { console.log(user);
   const perfilUsuario = document.getElementById('informacion-usuario');
-  const dbRefInfoUsuario = firebase.database().ref();
-  dbRefInfoUsuario.on('value', snap => 
-  perfilUsuario.innerHTML = JSON.stringify(snap.val(),null,1));
-  // console.log(dbRefInfoUsuario);
+  firebase.database().ref(`users/${user.uid}`) //ref es la ruta para llegar a los datos
+  .once('value')
+  .then((user)=>{
+    const nickname = user.val().nickName;
+    const nombre = user.val().nombre;
+    const edad = user.val().edad;
+    // console.log(user.val().nombre);
+    // console.log(user.val().nickName);
+    // console.log(user.val().edad);
+    perfilUsuario.innerHTML = `<h3>${nickname}</h3>`;
+  })
+  .catch((error)=>{
+      console.log("Database error > "+JSON.stringify(error));
+  });
 }
-perfil();
+
+window.onload = () =>{
+  firebase.auth().onAuthStateChanged( user =>{
+      if(user){ //Si está logeado, mostraremos la opción loggedIn
+          // console.log(user);
+          perfil(user);
+      }
+  });
+}
+ 
 
 // function estadoUsuario() {
 //   firebase.auth().onAuthStateChanged(firebaseUsers => {
