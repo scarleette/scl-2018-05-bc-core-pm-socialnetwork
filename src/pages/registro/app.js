d8540affda57f3ef4eeb9ec89737c687feeef1d4
Scarlett <scar.antoine@gmail.com>
2018-07-19 15:37:56 -0400

    Se eliminan comentarios de prueba

--- src/pages/registro/app.js
+++ src/pages/registro/app.js
@@ -10,11 +10,10 @@ formulario.addEventListener('submit', e =>  {
     const contrasena = document.getElementById('contrasena').value;
     if(nombre.length != 0 && apellido.length != 0 && edad.length != 0
     && correo.length != 0 && contrasena.length != 0 && nickName.length != 0) {
+        console.log('if')
         firebase.auth().createUserWithEmailAndPassword(correo, contrasena)
         .then(Response => {
-            // console.log(Response);
             const userId = Response.user.uid;
-            // console.log(userId);
             firebase.database().ref('users/' + userId).set( {
                nickName: nickName,
                nombre: nombre,
@@ -22,7 +21,7 @@ formulario.addEventListener('submit', e =>  {
                edad: edad,
                correo: correo 
             })
-            location.href = '../login/login.html';
         })
+       // location.href = '../login/login.html';
     }
 });
